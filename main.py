b2f5738fbd0d19cb47aa19d8bf5308830a9343ec
Nitiket <nitiketshinde2000@gmail.com>
2020-12-29 23:40:33 +0530

    1st_update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygame import mixer
-from DateTime import DateTime
+from datetime import datetime
 from time import time
 c = 0
 
@@ -13,13 +13,19 @@ def music(file):
     if q == "done":
         mixer.music.stop()
     elif q == "exit":
+        print("Thank You !!! , See you next time !")
         exit()
+
+def log(w):
+    f = open("log.txt", "a")
+    f.writelines(w)
+    f.close()
 
 if __name__ == '__main__':
     print("\n ++++++++++ : HEALTHY PROGRAMMER : ++++++++++")
     print("( We care for your thirst, eyes and physical health ... )")
     print("\n *MENU*")
-    print("\n1.Set drinking timer\n2.Set eye timer\n3.Set exercise timer\n4.")
+    print("\n1.Set drinking timer\n2.Set eye timer\n3.Set exercise timer\n")
 
     d = int(input("\nIn how many minutes you want to drink water ?\n"))
     local_timed = float(d)
@@ -38,15 +44,18 @@ if __name__ == '__main__':
     while True:
         if time() - timed > local_timed:
             music("drink.mp3")
+            w = "\n Water level covered at [ ", str(datetime.now()), " ]"
+            log(w)
             timed = time()
 
         if time() - timee > local_timee:
             music("eyes.mp3")
+            w = "\n Eyes Blinked at [ ", str(datetime.now()), " ]"
+            log(w)
             timee = time()
 
         if time() - timep > local_timep:
             music("exercise.mp3")
+            w = "\n Exercise done at [ ", str(datetime.now()), " ]"
+            log(w)
             timep = time()
-
-
-    print("Thank You !!! , See you next time !")
